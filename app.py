cb5e4ba2a98307800e4e2348bdba2a4261bbf5ab
lightAssemble <lightWavve@gmail.com>
2018-11-16 22:53:32 +0300

    fix...

--- app.py
+++ app.py
@@ -72,6 +72,7 @@ def get_message():
     except Exception as e:
         logging.error("Error during parsing message, " + str(e.args))
 
+if __name__ == '__main__':
 
-port = int(env.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    port = int(env.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
