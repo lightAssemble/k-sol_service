b100b06497cda5ad3e41de97500584a79ce1f110
lightAssemble <lightWavve@gmail.com>
2018-11-16 23:00:14 +0300

    fix parsing conn string

--- app_db.py
+++ app_db.py
@@ -2,7 +2,7 @@ from pony.orm import Database
 
 
 def _parse(cs: str):
-    user_pass, host_port_dbname = cs.split("@")
+    user_pass, host_port_dbname = cs.replace("postgres://", "").split("@")
     host_port, db_name = host_port_dbname.split("/")
     user, passw = user_pass.split(":")
     host, port = host_port.split(":")
@@ -20,5 +20,5 @@ def get(connection_string) -> Database:
     if connection_string:
         db.bind(provider='postgres', **_parse(connection_string))
     else:
-        db.bind(provider='sqlite', filename='./db.sqlite',create_db=True)
+        db.bind(provider='sqlite', filename='./db.sqlite', create_db=True)
     return db
